8d0b0fd0423f17279d644e6a3c04d20bc36ab7ac
FME <github@hilscher.com>
2018-08-30 12:34:19 +0200

    Mirror dts/netx4000 directory from kernel repository and adapt dts-files to use mdio-bitbang driver for PHY connections.

---
+++ files/dts/dt-bindings/video/netx4000.h
@@ -0,0 +1,13 @@
+#ifndef _DT_BINDINGS_FB_NETX4000_H
+#define _DT_BINDINGS_FB_NETX4000_H
+
+/* Pixel Mode */
+#define ARGB8888  0
+#define RGBA8888  1
+#define RGB565    2
+#define AL88      3  /* currently not supported */
+#define AL44      4  /* currently not supported */
+#define AL8       5  /* currently not supported */
+#define L8        6  /* currently not supported */
+
+#endif /* _DT_BINDINGS_FB_NETX4000_H */
